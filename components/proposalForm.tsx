2596d2b5266960a1b608b2aa47d67984d2cb9e34
Lana Butorovic <109984701+Lana-Butorovic@users.noreply.github.com>
2022-10-10 19:07:57 -0400

    Adding a "Submit for RFC" button (#4)
    * Adding a Submit for RFC button
    
    * Adding a corrected Submit for RFC button
    
    * Fixed alignment of the button

--- components/proposalForm.tsx
+++ components/proposalForm.tsx
@@ -109,6 +109,11 @@ const ProposalForm = () => {
           <Button colorScheme="blue" isLoading={isLoading} type="submit">
             Save
           </Button>
+          <Link href="#" passHref>
+          <Button colorScheme="blue" isLoading={isLoading}>
+            Submit for RFC
+          </Button>
+          </Link>
         </Flex>
       </VStack>
     </form>
