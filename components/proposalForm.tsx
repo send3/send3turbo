ce44b0c12f09f7c4fac2d8ac25c123a7576f8eb4
Kacey Wood <kacey.l.wood@ey.com>
2022-10-18 17:56:01 -0400

    BIPT-32 (#5)
    * BIPT-32
    
    * refactored New Proposal requirements and deleted Date Ratified
    
    * refactored bipt32
    
    * refactor: delete hstack

--- components/proposalForm.tsx
+++ components/proposalForm.tsx
@@ -10,6 +10,7 @@ import {
   Spacer,
   Textarea,
   VStack,
+  Select,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
@@ -33,72 +34,98 @@ const ProposalForm = () => {
     <form onSubmit={handleSubmit(onSubmit)}>
       <VStack align="stretch" spacing={6}>
         <FormControl isRequired isInvalid={!!errors.name}>
-          <FormLabel>Proposal Name</FormLabel>
+          <FormLabel>BIP-#</FormLabel>
           <InputGroup>
             <InputLeftAddon>BIP-#:</InputLeftAddon>
-            <Input
-              {...register("name", { required: true })}
-              autoComplete="off"
-            />
+            <Input {...register("id", { required: true })} autoComplete="off" />
           </InputGroup>
         </FormControl>
 
         <FormControl>
-          <FormLabel>Co-Authors</FormLabel>
-          <Input {...register("coAuthors")} autoComplete="off" />
-        </FormControl>
-
-        <FormControl isRequired>
-          <FormLabel>Summary</FormLabel>
-          <Textarea {...register("summary", { required: true })} />
-          <FormHelperText>
-            One to two sentances that describe the proposal at a high level.
-          </FormHelperText>
+          <FormLabel>Title of BIP:</FormLabel>
+          <Input {...register("name")} autoComplete="off" />
         </FormControl>
 
         <FormControl>
-          <FormLabel>Motivation</FormLabel>
-          <Textarea minH="10rem" {...register("motivation")} />
+          <FormLabel>Co-Authors:</FormLabel>
+          <FormHelperText>Enter full names of all co-authors.</FormHelperText>
+          <Input {...register("coAuthors")} autoComplete="off" />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Date Proposed:</FormLabel>
+          <Input
+            {...register("dateProposal")}
+            placeholder="Select Proposal Submission Date"
+            type="date"
+            autoComplete="off"
+          />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Championship Team:</FormLabel>
+          <FormHelperText>
+            Enter full names of volunteers supporting.
+          </FormHelperText>
+          <Input {...register("championshipTeam")} autoComplete="off" />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Leadership Sponsor:</FormLabel>
+          <FormHelperText>
+            Enter full name of Leadership Sponsor.
+          </FormHelperText>
+          <Input {...register("leadershipSponsor")} autoComplete="off" />
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel>Simple Summary/Abstract:</FormLabel>
+          <FormHelperText>
+            Provide one to two sentences that describe the proposal at a high
+            level.
+          </FormHelperText>
+          <Textarea minH="10rem" {...register("summary")} />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Motivation(s):</FormLabel>
           <FormHelperText>
             Clearly describe the problem statement and the value it adds. Show
             why this proposal is valuable to our practice.
           </FormHelperText>
+          <Textarea {...register("motivation")} />
         </FormControl>
 
         <FormControl>
           <FormLabel>Specifications</FormLabel>
-          <Textarea minH="10rem" {...register("specifications")} />
           <FormHelperText>
-            Detailed description of the proposal. Feel free to add rationale
-            explaining why certain design choices were made in the
+            Enter detailed description of the proposal. Feel free to add
+            rationale explaining why certain design choices were made in the
             specification.
           </FormHelperText>
-        </FormControl>
-
-        <FormControl>
-          <FormLabel>Timeline</FormLabel>
-          <Textarea {...register("timeline")} />
-          <FormHelperText>
-            Proposed Timeline and/or Defined Stages to accomplish the proposal
-            (Keep high level! We aren&apos;t expecting a full project plan here)
-          </FormHelperText>
+          <Textarea {...register("specifications")} />
         </FormControl>
 
         <FormControl>
           <FormLabel>Risks/Impediments</FormLabel>
-          <Textarea {...register("risks")} />
           <FormHelperText>
-            Document any potential risks that will slow or block this effort
+            Document any potential risks that will slow or block this effort.
           </FormHelperText>
+          <Textarea {...register("risks")} />
         </FormControl>
 
         <FormControl>
           <FormLabel>Success Metrics</FormLabel>
-          <Textarea {...register("successMetrics")} />
           <FormHelperText>
-            Metrics to measure success of the proposal once accepted and
-            actioned upon
+            Enter metrics that will be used to measure the success of the
+            proposal once accepted and actioned upon.
           </FormHelperText>
+          <Textarea {...register("successMetrics")} />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel>Status</FormLabel>
+          <Input {...register("status")} />
         </FormControl>
 
         <Flex gap={3}>
@@ -110,9 +137,7 @@ const ProposalForm = () => {
             Save
           </Button>
           <Link href="#" passHref>
-          <Button colorScheme="blue">
-            Submit for RFC
-          </Button>
+            <Button colorScheme="blue">Submit for RFC</Button>
           </Link>
         </Flex>
       </VStack>
