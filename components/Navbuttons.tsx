e4a1fbd7a86aeaac7b72594b2dba24bb7ec5490e
Lana Butorovic <lana.butorovic@ey.com>
2022-10-24 13:02:44 -0400

    Fixed the key prop error in the Navbuttons component file.

--- components/Navbuttons.tsx
+++ components/Navbuttons.tsx
@@ -6,7 +6,6 @@ import {
 } from "@chakra-ui/react";
 
 
-//Created an array of objects containing the names of the pages and href so that it can be dynamically pulled from the Button component in JSX
 const links = [
   {name:'All',href:'/'},
   {name:'Drafts',href:'/draft'},
@@ -17,14 +16,11 @@ const links = [
 const Navbuttons = (props: any) => {
 
     return(<div>
-        {/* React.Children.toArray is a code i added to get rid of a error in console regarding keys in map function  */}
         {React.Children.toArray(
-        links.map((lnk)=>
-            <ButtonGroup isAttached>
-                {/* Active button - Usage of props in order to get the page selected and setting the background color */}
-                <Button _hover={{ bg: '#ddeaf7' }} bg={lnk.name === props.page ? "#ddeaf7" : "" } >
-                    {/* Taking href and name from the array of objects above - links  */}
-                    <Link href={lnk.href}>{lnk.name}</Link>
+        links.map((lnk,idx)=>
+            <ButtonGroup key={idx} isAttached>
+                <Button key={idx}_hover={{ bg: '#ddeaf7' }} bg={lnk.name === props.page ? "#ddeaf7" : "" } >
+                    <Link key={idx} href={lnk.href}>{lnk.name}</Link>
                 </Button>
             </ButtonGroup>
         ))}
